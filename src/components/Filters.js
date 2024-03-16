d920de20de3f351cd7284d11152c2ef414423bcc
gayathriganesh96 <gayathrimanju7@gmail.com>
2024-03-16 20:51:10 +0530

    modal-popup

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -13,6 +13,8 @@ export default function Filters() {
     const [sortedMeals, setSortedMeals] = useState([]);
     const [isPopupOpen, setIsPopupOpen] = useState(false);
     const [modalMeal, setModalMeal] = useState(null);
+    const [foodItems, setFoodItems] = useState([]);
+    const [selectedFoodItem, setSelectedFoodItem] = useState(null);
 
     const toggleDropdown = () => {
         setIsOpen(!isOpen);
@@ -58,6 +60,7 @@ export default function Filters() {
                         const categoryData = await categoryResponse.json();
                         const category = categoryData.meals[0].strCategory;
                         const rating = (Math.random() * (5 - 3) + 3).toFixed(1);
+
 
                         return { ...meal, category, rating };
                     });
@@ -171,20 +174,57 @@ export default function Filters() {
                 {meals !== null && (
                     meals.map(meal => (
                         <div key={meal.idMeal}>
-                            <Meal key={meal.idMeal} meal={meal} />
+                            <Meal key={meal.idMeal} meal={meal} toggleModal={toggleModal} />
                         </div>
                     ))
 
                 )}
             </div>
             {isPopupOpen && (
-                <div className="fixed top-0 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center">
-                    <div className="bg-white p-8 rounded-lg">
-                        {/* Display modal content with meal details */}
-                        <h2 className="text-2xl font-bold mb-4">{modalMeal.strMeal}</h2>
+                <div className="fixed top-5 left-0 w-full h-full bg-black bg-opacity-50 flex justify-center items-center" onClick={toggleModal}>
+                    <div className="bg-white p-8 rounded-lg " onClick={(e) => e.stopPropagation()}>
+                        <div className=' relative'>
+                            <h2 className="text-2xl font-bold mb-4">{modalMeal.strMeal}</h2>
+                            <button
+                                type="button"
+                                class="absolute top-0 right-0 p-2"
+                                data-twe-modal-dismiss
+                                onClick={toggleModal}
+                                aria-label="Close">
+                                <span class="[&>svg]:h-6 [&>svg]:w-6">
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="#333333"
+                                        viewBox="0 0 24 24"
+                                        stroke-width="1.5"
+                                        stroke="currentColor">
+                                        <path
+                                            stroke-linecap="round"
+                                            stroke-linejoin="round"
+                                            d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </span>
+                            </button>
+                        </div>
                         <img src={modalMeal.strMealThumb} alt={modalMeal.strMeal} className="w-full mb-4" />
-                        <p>{modalMeal.strInstructions}</p>
-                        <button onClick={toggleModal} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">Close Modal</button>
+
+                        {/* <h3 className="antialiased font-bold text-lg break-words">{modalMeal.strMeal}</h3> */}
+                        <div className="meals-rating flex">
+                            <svg width="20" height="20" viewBox="0 0 20 20" fill="none" role="img" aria-hidden="true">
+                                <circle cx="10" cy="10" r="9" fill="url(#paint0_linear)"></circle>
+                                <path d="M10.0816 12.865C10.0312 12.8353 9.96876 12.8353 9.91839 12.865L7.31647 14.3968C6.93482 14.6214 6.47106 14.2757 6.57745 13.8458L7.27568 11.0245C7.29055 10.9644 7.26965 10.9012 7.22195 10.8618L4.95521 8.99028C4.60833 8.70388 4.78653 8.14085 5.23502 8.10619L8.23448 7.87442C8.29403 7.86982 8.34612 7.83261 8.36979 7.77777L9.54092 5.06385C9.71462 4.66132 10.2854 4.66132 10.4591 5.06385L11.6302 7.77777C11.6539 7.83261 11.706 7.86982 11.7655 7.87442L14.765 8.10619C15.2135 8.14085 15.3917 8.70388 15.0448 8.99028L12.7781 10.8618C12.7303 10.9012 12.7095 10.9644 12.7243 11.0245L13.4225 13.8458C13.5289 14.2757 13.0652 14.6214 12.6835 14.3968L10.0816 12.865Z" fill="white"></path>
+                                <defs>
+                                    <linearGradient id="paint0_linear" x1="10" y1="1" x2="10" y2="19" gradientUnits="userSpaceOnUse">
+                                        <stop stop-color="#21973B"></stop>
+                                        <stop offset="1" stop-color="#128540"></stop>
+                                    </linearGradient>
+                                </defs>
+                            </svg>
+                            <span className='ml-1 font-bold text-base'>{modalMeal.rating}</span>
+                        </div>
+                        <p className="text-gray-500 mt-1 text-base">{modalMeal.category}</p>
+
+                        {/* <button onClick={toggleModal} className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">Close Modal</button> */}
                     </div>
                 </div>
             )}
